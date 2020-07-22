e13e0e4dabddd5198717e242c3580c04f009a1f5
Kennie6908 <60665094+Kennie6908@users.noreply.github.com>
2020-07-22 18:35:59 -0400

    uploading kata solutions

---
+++ 8 kyu/sum-arrays.py
@@ -0,0 +1,10 @@
+#Write a method sum (sum_array in python, sumarray in julia, SumArray in C#) that takes an array of numbers and returns the sum of the numbers. 
+#These may be integers or decimals for Ruby and any instance of Num for Haskell. The numbers can also be negative. 
+#If the array does not contain any numbers then you should return 0.
+
+def sum_array(a):
+    b = 0
+    for x in a:
+        b += x
+    return b
+        
